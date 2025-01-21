5ead44bcabdd29a775e9ad2206967fcd33680814
mchirkov <mchirkov@ptsecurity.com>
2025-01-21 18:23:01 +0300

    Версия 4.28

--- tests/test_find_element_by.py
+++ tests/test_find_element_by.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.by import By
 
 def test_login_page(browser):
     browser.get(browser.url + "/administration")
-    browser.find_element(By.ID, "input-pass")
+    browser.find_element(By.ID, "input-username")
     browser.find_element(By.NAME, "password")
     browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    browser.find_element(By.LINK_TEXT, "Forgotten Password")
     browser.find_element(By.XPATH, "//*[text()='OpenCart']")
     time.sleep(2) # Для демонстрации
--- tests/test_using_by_class.py
+++ tests/test_using_by_class.py
@@ -2,9 +2,8 @@ from pages import LoginAdminPage
 
 
 def test_login_page_external(browser):
-    browser.get(browser.url + "/admin")
+    browser.get(browser.url + "/administration")
     browser.find_element(*LoginAdminPage.USERNAME_INPUT)
     browser.find_element(*LoginAdminPage.PASSWORD_INPUT)
     browser.find_element(*LoginAdminPage.SUBMIT_BUTTON)
-    browser.find_element(*LoginAdminPage.FORGOTTEN_PASSWORD)
     browser.find_element(*LoginAdminPage.OPENCART_LINK)
